a147ed2287b90d63809439152bb3b0ac8224af46
M1g3L14Ka <147346501+M1g3L14Ka@users.noreply.github.com>
2024-12-11 02:16:56 +0300

    itpm_laba1_4kyrs

--- laba1a_4kyrs/main.py
+++ laba1a_4kyrs/main.py
@@ -27,18 +27,21 @@ engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
+
 # Определение модели SQLAlchemy для пользователя
 class User(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True, index=True)
     username = Column(String(50), unique=True, index=True)
-    email = Column(String(100), unique=True, index=True)
+    email = Column(String(50), unique=True, index=True)
     full_name = Column(String(100), nullable=True)
     hashed_password = Column(String(100))
     disabled = Column(Boolean, default=False)
 
+
 # Создание таблиц в базе данных
 Base.metadata.create_all(bind=engine)
+
 
 # Определение Pydantic модели для пользователя
 class UserCreate(BaseModel):
@@ -47,12 +50,14 @@ class UserCreate(BaseModel):
     full_name: str | None = None
     password: str
 
+
 class UserUpdate(BaseModel):
     username: str | None = None
     email: str | None = None
     full_name: str | None = None
     password: str | None = None
     disabled: bool | None = None
+
 
 class UserResponse(BaseModel):
     id: int
@@ -64,15 +69,19 @@ class UserResponse(BaseModel):
     class Config:
         from_attributes = True
 
+
 class UserInDB(UserResponse):
     hashed_password: str
+
 
 class Token(BaseModel):
     access_token: str
     token_type: str
 
+
 class TokenData(BaseModel):
     username: str | None = None
+
 
 # Зависимость для получения сессии базы данных
 def get_db():
@@ -82,17 +91,21 @@ def get_db():
     finally:
         db.close()
 
+
 def read_user(user_id: int, db: Session = Depends(get_db)):
     user = db.query(User).filter(User.id == user_id).first()
     if user is None:
-        raise HTTPException(status_code=404, detail="User not found")
+        raise HTTPException(status_code=404, detail="Пользователь не найден")
     return user
+
 
 def hash_password(password: str) -> str:
     return pwd_context.hash(password)
 
+
 def verify_password(plain_password, hashed_password):
     return pwd_context.verify(plain_password, hashed_password)
+
 
 def authenticate_user(db: Session, username: str, password: str):
     user = get_user_by_username(db, username)
@@ -101,6 +114,7 @@ def authenticate_user(db: Session, username: str, password: str):
     if not verify_password(password, user.hashed_password):
         return False
     return user
+
 
 def create_access_token(data: dict, expires_delta: timedelta | None = None):
     to_encode = data.copy()
@@ -112,14 +126,16 @@ def create_access_token(data: dict, expires_delta: timedelta | None = None):
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
 
+
 def get_user_by_username(db: Session, username: str):
     return db.query(User).filter(User.username == username).first()
+
 
 # Получение текущего пользователя по токену
 async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)], db: Session = Depends(get_db)):
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="Could not validate credentials",
+        detail="Не удалось подтвердить учетные данные",
         headers={"WWW-Authenticate": "Bearer"},
     )
     try:
@@ -135,30 +151,35 @@ async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)], db: Se
         raise credentials_exception
     return user
 
+
 async def get_current_active_user(current_user: Annotated[User, Depends(get_current_user)]):
     if current_user.disabled:
-        raise HTTPException(status_code=400, detail="Inactive user")
+        raise HTTPException(status_code=400, detail="Неактивный пользователь")
     return current_user
+
 
 def get_users(db: Session = Depends(get_db)):
     users = db.query(User).all()
     if not users:
-        raise HTTPException(status_code=404, detail="Users not found")
+        raise HTTPException(status_code=404, detail="Пользователь не найден")
     return users
+
 
 def get_user(user_id: int, db: Session = Depends(get_db)):
     user = db.query(User).filter(User.id == user_id).first()
     if user is None:
-        raise HTTPException(status_code=404, detail="User not found")
+        raise HTTPException(status_code=404, detail="Пользователь не найден")
     return user
+
 
 # Маршрут для получения пользователя по ID
 @app.get("/users/", response_model=list[UserResponse])
 async def get_users(current_user: Annotated[User, Depends(get_current_active_user)], db: Session = Depends(get_db)):
     users = db.query(User).all()
     if not users:
-        raise HTTPException(status_code=404, detail="Users not found")
+        raise HTTPException(status_code=404, detail="Пользователь не найден")
     return users
+
 
 # Маршрут для удаления пользователя по ID
 @app.delete("/users/{user_id}", response_model=UserResponse)
@@ -166,18 +187,20 @@ async def delete_user(user_id: int, current_user: Annotated[User, Depends(get_cu
                       db: Session = Depends(get_db)):
     user = db.query(User).filter(User.id == user_id).first()
     if user is None:
-        raise HTTPException(status_code=404, detail="User not found")
+        raise HTTPException(status_code=404, detail="Пользователь не найден")
     db.delete(user)
     db.commit()
     return user
 
+
 # Маршрут для обновления пользователя
 @app.put("/users/{user_id}", response_model=UserResponse)
 async def update_user(user_id: int, user_update: UserUpdate,
-                      current_user: Annotated[User, Depends(get_current_active_user)], db: Session = Depends(get_db)):
+                      current_user: Annotated[User, Depends(get_current_active_user)],
+                      db: Session = Depends(get_db)):
     user = db.query(User).filter(User.id == user_id).first()
     if user is None:
-        raise HTTPException(status_code=404, detail="User not found")
+        raise HTTPException(status_code=404, detail="Пользователь не найден")
     if user_update.username:
         user.username = user_update.username
     if user_update.email:
@@ -194,18 +217,21 @@ async def update_user(user_id: int, user_update: UserUpdate,
         return user
     except IntegrityError:
         db.rollback()
-        raise HTTPException(status_code=400, detail="Username or Email already registered")
+        raise HTTPException(status_code=400, detail="Пользователь с таким именем или почтой уже существует")
     except Exception as e:
         db.rollback()
         raise HTTPException(status_code=500, detail=str(e))
+
 
 @app.get("/items/")
 async def read_items(token: Annotated[str, Depends(oauth2_scheme)]):
     return {"token": token}
 
+
 @app.get("/users/me", response_model=UserResponse)
 async def read_users_me(current_user: Annotated[User, Depends(get_current_active_user)]):
     return current_user
+
 
 @app.post("/token", response_model=Token)
 async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
@@ -213,7 +239,7 @@ async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(
     if not user:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Incorrect username or password",
+            detail="Неверное имя пользователя или пароль",
             headers={"WWW-Authenticate": "Bearer"},
         )
     access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
@@ -221,6 +247,7 @@ async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(
         data={"sub": user.username}, expires_delta=access_token_expires
     )
     return {"access_token": access_token, "token_type": "bearer"}
+
 
 @app.post("/register/", response_model=UserResponse)
 async def register_user(user: UserCreate, db: Session = Depends(get_db)):
@@ -238,7 +265,7 @@ async def register_user(user: UserCreate, db: Session = Depends(get_db)):
         return db_user
     except IntegrityError:
         db.rollback()
-        raise HTTPException(status_code=400, detail="Username or Email already registered!")
+        raise HTTPException(status_code=400, detail="Пользователь с таким именем или почтой уже существует")
     except Exception as e:
         db.rollback()
         raise HTTPException(status_code=500, detail=str(e))
